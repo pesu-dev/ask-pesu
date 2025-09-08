833086601b056f630ac137dfcf4c65ca28bada7f
aditeyabaral <aditeya.baral@gmail.com>
2025-09-08 22:00:37 +0530

    Add frontend and Dockerfile Deployment

--- frontend/app/utils/query.js
+++ frontend/app/utils/query.js
@@ -1,23 +1,23 @@
 import { toast } from "sonner"
 
 export default async function Query(question) {
-	const url = new URL(process.env.NEXT_PUBLIC_HF_API_URL)
-	url.pathname = "/ask"
+	try {
+		const resp = await fetch("/ask", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ query: question }),
+		})
 
-	const resp = await fetch(url, {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({
-			query: question,
-		}),
-	})
+		if (resp.ok) {
+			return await resp.json()
+		}
 
-	if (resp.ok) {
-		return await resp.json()
+		toast.error("An error occurred. Check browser console for logs.")
+		console.error("Request failed:", resp.status, resp.statusText)
+	} catch (err) {
+		toast.error("Failed to reach the server.")
+		console.error("Network error:", err)
 	}
-
-	toast.error("An error occurred. Check browser console for logs.")
-	console.error(resp)
 }
